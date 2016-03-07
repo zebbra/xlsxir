8c563e9d3b53f858b06b16b4100fb0f2de1de048
Jason Kennell <kennellroxco@gmail.com>
2016-03-07 06:35:15 -0700

    Created Unzip module

--- lib/xlsxir/parse.ex
+++ lib/xlsxir/parse.ex
@@ -1,4 +1,5 @@
 defmodule Xlsxir.Parse do
+  import Xlsxir.Unzip, only: [extract_xml: 2]
   import SweetXml
   
   def shared_strings_with_index(path) do
@@ -24,25 +25,5 @@ defmodule Xlsxir.Parse do
             ] 
           ]
         )
-  end
-
-  defp extract_xml(path, inner_path) do
-    path
-    |> to_char_list
-    |> :zip.zip_open([:memory])
-    |> case do
-      {:error, cause}      -> {:error, cause}
-      {:ok, zip_directory} ->
-        inner_path
-        |> :zip.zip_get(zip_directory)
-        |> case do
-          {:error, cause} -> {:error, cause}
-          {:ok, {_, file_content}} ->
-            case :zip.zip_close(zip_directory) do
-              {:error, cause} -> {:error, cause}
-              :ok             -> {:ok, file_content}
-            end
-        end
-    end
   end
 end
--- lib/xlsxir/unzip.ex
+++ lib/xlsxir/unzip.ex
@@ -1,5 +1,23 @@
 defmodule Xlsxir.Unzip do
-  
-  
-  
+
+  def extract_xml(path, inner_path) do
+    path
+    |> to_char_list
+    |> :zip.zip_open([:memory])
+    |> case do
+      {:error, cause}      -> {:error, cause}
+      {:ok, zip_directory} ->
+        inner_path
+        |> :zip.zip_get(zip_directory)
+        |> case do
+          {:error, cause} -> {:error, cause}
+          {:ok, {_, file_content}} ->
+            case :zip.zip_close(zip_directory) do
+              {:error, cause} -> {:error, cause}
+              :ok             -> {:ok, file_content}
+            end
+        end
+    end
+  end
+
 end

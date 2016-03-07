e7428247036a73284f1c93414dceacf26acfe998
kennellroxco <kennellroxco@gmail.com>
2016-03-07 14:49:16 -0700

    Refactored worksheet fx in Parse module

--- lib/xlsxir/parse.ex
+++ lib/xlsxir/parse.ex
@@ -1,7 +1,7 @@
 defmodule Xlsxir.Parse do
   import Xlsxir.Unzip, only: [extract_xml: 2]
   import SweetXml
-  
+
   def shared_strings_with_index(path) do
     {:ok, strings} = extract_xml(path, 'xl/sharedStrings.xml')
     strings
@@ -10,20 +10,25 @@ defmodule Xlsxir.Parse do
   end
 
   def worksheet(path, index) do
-    {:ok, worksheet} = extract_xml(path, 'xl/worksheets/sheet#{index + 1}.xml')
-    worksheet
-    |> xmap(
-        sheet: [
-          ~x"//row"l,
-          row: ~x"./@r",
-          columns: [
-            ~x"./c"l,
-            column:   ~x"./@r",
-            type:     ~x"./@t",
-            function: ~x"./f/text()"o,
-            value:    ~x"./v/text()"
-            ] 
-          ]
-        )
+    {:ok, sheet} = extract_xml(path, 'xl/worksheets/sheet#{index + 1}.xml')
+
+    sheet
+    |> xpath(~x"//row/@r"l)
+    |> Enum.reduce(%{}, fn(row, acc) -> Map.put(acc, row,
+      sheet
+      |> xpath(~x"//row[contains(@r, '#{row}')]/c/@r"l)
+      |> Enum.reduce(%{}, fn(cell, acc) -> Map.put(acc, cell,
+          [      
+            col_data(sheet, cell, "@t"),
+            col_data(sheet, cell, "@s"),
+            col_data(sheet, cell, "f/text()", "o"),
+            col_data(sheet, cell, "v/text()")
+          ])
+        end)
+      )end)
   end
+
+  defp col_data(xml, cell, data, opt \\ "") do
+    xpath(xml, ~x"//c[contains(@r, '#{cell}')]/#{data}"opt)
+  end  
 end

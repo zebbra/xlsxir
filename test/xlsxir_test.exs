3a0f6d4a282a24c6a05a04f4564156c1a21e2ebd
Tricote <thibaut.decaudain@gmail.com>
2017-08-30 20:00:35 +0200

    Fix sax parser test

--- test/xlsxir_test.exs
+++ test/xlsxir_test.exs
@@ -74,8 +74,4 @@ defmodule XlsxirTest do
     assert 9 == Enum.count(res)
     assert [["string one", "string two", 10, 20, {2016, 1, 1}]] == get_list(tid)
   end
-
-  test "stream_list returns a stream of rows" do
-    assert %Stream{} = stream_list(path(), 1)
-  end
 end
